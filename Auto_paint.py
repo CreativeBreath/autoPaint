eebb3618c714eb7e7f32501a8275ba708377d8ba
CreativeBreath <37840691+CreativeBreath@users.noreply.github.com>
2018-03-27 20:40:57 +0600

    adding Autopaint.py

---
+++ Auto_paint.py
@@ -0,0 +1,63 @@
+import pyautogui, time
+
+
+time.sleep(3)
+m = pyautogui.click(29,747)
+
+
+
+pyautogui.typewrite('Paint', 0.5)
+time.sleep(1)
+
+pyautogui.typewrite(['enter'])
+
+time.sleep(12)
+
+
+
+
+pyautogui.keyDown('altleft')
+pyautogui.press('space')
+pyautogui.keyUp('altleft')
+pyautogui.press('down')
+pyautogui.press('down')
+pyautogui.press('down')
+pyautogui.press('down')
+pyautogui.press('enter')
+
+time.sleep(4)
+
+pyautogui.click(41,175)
+
+distance = 200
+x = 0
+while distance > 0:
+    x = x + 0.2
+    pyautogui.dragRel(distance, 0, duration=x)
+    distance = distance - 5
+    x = x + 0.2
+    pyautogui.dragRel(0, distance, duration=x)
+    x = x - 0.2
+    pyautogui.dragRel(-distance, 0, duration=x)
+    distance = distance - 5
+    x = x = 0.1
+    pyautogui.dragRel(0, -distance, duration=x)
+
+
+pyautogui.click(250,175)
+
+distance2 = 200
+
+
+
+
+time.sleep(1)
+
+pyautogui.keyDown('ctrlleft')
+pyautogui.press('s')
+pyautogui.keyUp('ctrlleft')
+time.sleep(2)
+pyautogui.typewrite('this is awesome', 1)
+time.sleep(1)
+pyautogui.press('enter')
+
